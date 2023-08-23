7ee32fe5e851aee7d1bdc0319a89751ee1275b6d
Yasser Omar Jammeli <yasser.omar.jammeli@gmail.com>
2023-08-23 10:41:29 +0200

    add app used colors to colors values class

---
+++ lib/core/utils/ui_utils/values/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color blue1C43CB = Color(0xFF1C43CB);
+const Color grey969DAA = Color(0xFF969DAA);
+const Color greyE7EDFC = Color(0xFFE7EDFC);
+const Color whiteFFFFFF = Color(0xFFFFFFFF);
+const Color black000000 = Color(0xFF000000);
