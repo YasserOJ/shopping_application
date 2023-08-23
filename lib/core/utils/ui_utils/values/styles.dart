1b2953e6c53781aebb8d51c23ea188c928c6636e
Yasser Omar Jammeli <yasser.omar.jammeli@gmail.com>
2023-08-23 10:42:02 +0200

    add app used text styles to styles class

---
+++ lib/core/utils/ui_utils/values/styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+TextStyle boldTextStyle = const TextStyle(
+  fontWeight: FontWeight.bold,
+);
+TextStyle normalTextStyle = const TextStyle(
+  fontWeight: FontWeight.normal,
+);
